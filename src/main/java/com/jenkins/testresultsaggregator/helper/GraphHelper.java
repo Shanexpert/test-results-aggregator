819981d36ae4bf43f4ca6a1ba3ff7c4fc93f368f
sokratis.sidereas <sokratis.sidereas@ssidereas.internal.upstreamsystems.com>
2019-10-14 13:11:53 +0300

    RM : chart title

--- src/main/java/com/jenkins/testresultsaggregator/helper/GraphHelper.java
+++ src/main/java/com/jenkins/testresultsaggregator/helper/GraphHelper.java
@@ -45,7 +45,7 @@ public class GraphHelper {
 	
 	public static JFreeChart createChartJob(final StaplerRequest req, CategoryDataset dataset) {
 		final JFreeChart chart = ChartFactory.createStackedAreaChart(
-				"Jobs", // chart title
+				null, // chart title
 				null, // unused
 				"Jobs Count", // range axis label
 				dataset, // data
@@ -132,7 +132,7 @@ public class GraphHelper {
 	
 	public static JFreeChart createChartTests(final StaplerRequest req, CategoryDataset dataset) {
 		final JFreeChart chart = ChartFactory.createStackedAreaChart(
-				"Tests", // chart title
+				null, // chart title
 				null, // unused
 				"Test Count", // range axis label
 				dataset, // data
