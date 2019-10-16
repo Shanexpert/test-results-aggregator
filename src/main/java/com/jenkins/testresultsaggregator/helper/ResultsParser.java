1400f0ceca042c468f7a581e57f8d4468793da75
sokratis.sidereas <sokratis.sidereas@ssidereas.internal.upstreamsystems.com>
2019-10-16 16:52:41 +0300

    FIX : catch exception

--- src/main/java/com/jenkins/testresultsaggregator/helper/ResultsParser.java
+++ src/main/java/com/jenkins/testresultsaggregator/helper/ResultsParser.java
@@ -1,15 +1,18 @@
 package com.jenkins.testresultsaggregator.helper;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.jenkins.testresultsaggregator.TestResultsAggregatorProjectAction;
 import com.jenkins.testresultsaggregator.data.AggregatedDTO;
@@ -137,7 +140,7 @@ public class ResultsParser {
 							}
 						}
 					}
-				} catch (Exception ex) {
+				} catch (ParserConfigurationException | SAXException | IOException ex) {
 					
 				}
 			}
